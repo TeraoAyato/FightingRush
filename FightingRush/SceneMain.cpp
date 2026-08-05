f70c246941af454f837120d665d50d0b6484c162
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-08-05 17:01:52 +0900

    背景途中

--- FightingRush/SceneMain.cpp
+++ FightingRush/SceneMain.cpp
@@ -48,8 +48,8 @@ void SceneMain::Draw()
 	
 	m_bg.Draw();
 #ifdef _DEBUG
-	DrawString(0, 0, "SceneMain", GetColor(255, 255, 255));
-	DrawFormatString(0, 16, GetColor(255, 255, 255), "FRAME:%d", m_frameCount);
+	DrawString(0, 0, "SceneMain", GetColor(0, 255, 0));
+	DrawFormatString(0, 16, GetColor(0, 255, 0), "FRAME:%d", m_frameCount);
 #endif
 	
 	m_enemy.Draw();
