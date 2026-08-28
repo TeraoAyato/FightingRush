080310444f7556f9e6884c74ffdcfae6f07d43c5
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-08-29 00:55:36 +0900

    exeアイコン作成

--- FightingRush/main.cpp
+++ FightingRush/main.cpp
@@ -21,6 +21,7 @@ namespace
 // プログラムは WinMain から始まります
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+	SetWindowIconID(333);
 	// windowモードで起動する
 	ChangeWindowMode(TRUE);
 
