2d7ad0e8f8ba081d34f5786826d0922d477c0fec
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-08-28 16:41:21 +0900

    微変更

--- FightingRush/main.cpp
+++ FightingRush/main.cpp
@@ -22,7 +22,7 @@ namespace
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	// windowモードで起動する
-	ChangeWindowMode(1);
+	ChangeWindowMode(TRUE);
 
 	// ウィンドウサイズを16:9に
 	SetGraphMode(Game::kScreenWidth, Game::kScreenHeight, Game::kColorDepth);
